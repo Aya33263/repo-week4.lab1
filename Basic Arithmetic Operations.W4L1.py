02548033ef53a559d228cac98350db63511b4b17
Aya33263 <ayaalzwghaibi@gmail.com>
2026-03-12 12:08:09 +1300

    add and multi.W4L1.py

--- Basic Arithmetic Operations.W4L1.py
+++ Basic Arithmetic Operations.W4L1.py
@@ -1,21 +1,33 @@
 # Basic Arithmetic Operations
-def add (a, b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def multiply (x,c):
-    return x*c
+def multiply(x, y):
+    return x * y
 
-def add_and_multiply (a, b, c):
-    sum_result =add (a, b)  #calling the add function
+def add_and_multiply(num1, num2, num3): 
 
-    # Making it flexible with input()
-    # Note: input() always returns text, so we use float() to convert it to a number
-    #print("Enter three numbers to add the first two, then multiply by the third.")
+    #add process
 
-    a = float(input("Enter first number (a): "))
-    b = float(input("Enter second number (b): "))
-    c = float(input("Enter third number (c): "))
-    product_result = multiply (sum_result, c)  #calling the multiply function
-    return product_result
+    sum_val = add(num1, num2)
+    
+    #multiply process
+    result = multiply(sum_val, num3)
+    
+    return result
 
-    print(result )  
+# this part for user 
+
+print(" HELLO Enter three numbers to add the first two, then multiply by the third.\n")
+
+# enter the numbers
+
+a = float(input("Enter first number (a): "))
+b = float(input("Enter second number (b): "))
+c = float(input("Enter third number (c): "))
+
+# call the function 
+final_result = add_and_multiply(a, b, c)
+
+# for the result
+print(final_result)
