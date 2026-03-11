17bdeb60c829056b0b9045f4e754d4915ae8fe92
Aya33263 <ayaalzwghaibi@gmail.com>
2026-03-12 11:47:37 +1300

    Add files via upload

---
+++ Adding Two Numbers.W4L1.py
@@ -0,0 +1,12 @@
+#Adding Two Numbers
+
+def add_number(a,b):
+    return a+ b
+a = float (input("enter your number as a : "))
+b = float (input("enter your number as b: "))
+
+result1 = add_number(a, b)
+result2 = add_number(a, b)
+
+print (result1)
+print (result2)
