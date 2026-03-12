47c470c8fbd3992f12d853151219a8d39322038d
Aya33263 <ayaalzwghaibi@gmail.com>
2026-03-13 10:11:49 +1300

    Add files via upload

---
+++ global counter with return.W4L2.py
@@ -0,0 +1,32 @@
+# Initialize a global counter
+
+registration_counter = 50000
+
+def student_registration():
+    global registration_counter 
+    # Use the global counter  to connected a date from outside and use it inside the def
+    
+    # Generate the Registration ID using the current counter value
+
+    registration_id = registration_counter
+    registration_counter += 1   # Increment the counter for the next registration
+      
+    #this part for the user
+      
+    date = input("Enter the registration date (dd/mm/yyyy): ")
+    student_name = input("Enter the Student Name: ")
+    student_id = input("Enter the Student ID: ")
+    course_name = input("Enter the Course Name: ")
+    
+    return date ,student_name, student_id, course_name,registration_id 
+#call the funcation and grt date before the print because we use "return" in this code
+ 
+date, student_name, student_id, course_name, registration_id = student_registration()
+
+# Display the information
+print("\nTHANKS FOR YOUR WAIT \nThe Student Registration Information : ")
+print(f"Date: {date}")
+print(f"Student Name: {student_name}")
+print(f"Student ID: {student_id}")
+print(f"Course Name: {course_name}")
+print(f"Registration ID: {registration_id}")
